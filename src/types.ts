a64e1292eed108a7c250009f46ce83bd1fb54abb
dubieltomasz <dubieltomasz420@gmail.com>
2026-06-24 21:14:46 +0200

    FEAT: Added data types for response

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export enum CleanEnergySource {
+    Biomass = 'biomass',
+    Nuclear = 'nuclear',
+    Hydro = 'hydro',
+    Wind = 'wind',
+    Solar = 'solar',
+};
+
+export interface GenerationMixItem {
+    fuel: string;
+    perc: number;
+};
+
+export interface GenerationInterval {
+    from: string;
+    to: string;
+    generationmix: GenerationMixItem[];
+};
+
+export interface GenerationResponse {
+    data: GenerationInterval;
+};
